5b93780e4170a2275445e796771c806f5e541374
Vasulevus <Vasulevus@gmail.com>
2022-04-19 20:20:52 +0300

    Update start.py

--- python/start.py
+++ python/start.py
@@ -5,36 +5,14 @@ import matplotlib.pyplot as pl
 import random
 import string
 
-# def dataset_creator(Name1, Name2, Name3, Name4, Number):
-#     A = []
-#     B = []
-#     letters = string.ascii_lowercase
-#     rand_letters = random.choices(letters,k=Number)
-#     rand_letters2 = random.choices(letters,k=Number)
-#     i = 0
-#     j = 0
-#     for letter in rand_letters:
-#         i = i + 1
-#         A.append(rand_letters[i-1])
-#     for letter in rand_letters2:
-#         j = j + 1
-#         B.append(rand_letters2[j-1])
-#     df = pd.DataFrame({Name1: A, Name2: B, Name3: np.random.randn(Number), Name4: np.round(np.random.normal(1,Number,Number))})
-# dataset_creator("A","B","C","D",10)
+letters = ['a']*4 + ['b']*4
+randomA = np.random.randn(8)
+randomB = np.random.randn(8)
+df = pd.DataFrame({'category': letters,
+                    'data': randomA,
+                    'weights': randomB})
+grouped = df.groupby(['category'])
 
-A = []
-B = []
-Number = 50
-letters = string.ascii_lowercase
-rand_letters = random.choices(letters,k=Number)
-rand_letters2 = random.choices(letters,k=Number)
-i = 0
-j = 0
-for letter in rand_letters:
-    i = i + 1
-    A.append(rand_letters[i-1])
-for letter in rand_letters2:
-    j = j + 1
-    B.append(rand_letters2[j-1])
-df = pd.DataFrame({"FirstName": A, "SecondName": B, 'data1': np.random.randn(Number), 'data2': np.round(np.random.normal(1,Number,Number))})
-df.groupby(["FirstName", "SecondName"]).sum()
+get_Wavg = lambda a : np.average(a['data'], weights = a['weights'])
+print(grouped)
+grouped.apply(get_Wavg)
