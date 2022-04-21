cf7960409f4edd4dd9b090dda43f51b5db62dd10
Vasulevus <Vasulevus@gmail.com>
2022-04-21 16:13:39 +0300

    SSAS start

---
+++ python/new.py
@@ -0,0 +1,27 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as pl
+import random
+import string
+
+
+
+def dataset(Name1, Name2, Name3, Name4, Number):
+    A = []
+    B = []
+    letters = string.ascii_lowercase
+    rand_letters = random.choices(letters,k=Number)
+    rand_letters2 = random.choices(letters,k=Number)
+    i = 0
+    j = 0
+    for letter in rand_letters:
+        i = i + 1
+        A.append(rand_letters[i-1])
+    for letter in rand_letters2:
+        j = j + 1
+        B.append(rand_letters2[j-1])
+    df = pd.DataFrame({Name1: A, Name2: B, Name3: np.random.randn(Number), Name4: np.round(np.random.normal(1,Number,Number))})
+
+dataframe = dataset("Start", "Low", "Mid", "High", 100)
+dataframe
+#dataframe.groupbyagg('mean')
--- python/start.py
+++ python/start.py
@@ -1,18 +1,21 @@
 
+from tkinter.font import names
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as pl
 import random
 import string
+from datetime import date, datetime
 
-letters = ['a']*4 + ['b']*4
-randomA = np.random.randn(8)
-randomB = np.random.randn(8)
-df = pd.DataFrame({'category': letters,
-                    'data': randomA,
-                    'weights': randomB})
-grouped = df.groupby(['category'])
+now = datetime.now()
+today = date.today()
+print(now)
+print(today)
+longer_ts = pd.Series(np.random.randn(1000), index=pd.date_range('1/1/2000', periods=1000))
+longer_ts['2001-02']
 
-get_Wavg = lambda a : np.average(a['data'], weights = a['weights'])
-print(grouped)
-grouped.apply(get_Wavg)
+index = pd.DataFrame(pd.date_range('2000-01-01', '2000-12-01', freq='D'))
+ts = pd.Series(np.random.randn(4), index=pd.date_range('1/1/2000', periods=4, freq='M'))
+index
+print(ts)
+ts.shift(-2)
