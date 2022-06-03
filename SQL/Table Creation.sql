4ff02378d120fcba08aabd310e3e3da3e9e65522
Vasulevus <Vasulevus@gmail.com>
2022-06-03 17:45:38 +0300

    add ssis files

---
+++ SQL/Table Creation.sql
@@ -0,0 +1,24 @@
+USE GZ_Arh;
+GO
+
+CREATE  TABLE [dbo].[05356](
+	[template] [nvarchar](max) NOT NULL,
+	[organization id] [nvarchar](max) NOT NULL,
+	[status] [nvarchar](max) NOT NULL,
+	[timestamp] [datetime] NOT NULL,
+	[Last Update] [datetime] NOT NULL,
+	[0] [nvarchar](max) NOT NULL,
+	[1] [numeric](18, 0) NULL,
+	[2] [numeric](18, 0) NULL,
+	[3] [numeric](18, 0) NULL,
+	[4] [numeric](18, 0) NULL,
+	[5] [numeric](18, 0) NULL,
+	[6] [numeric](18, 0) NULL,
+	[7] [numeric](18, 0) NULL,
+	[8] [numeric](18, 0) NULL,
+	[9] [numeric](18, 0) NULL,
+	[10] [numeric](18, 0) NULL,
+	[11] [numeric](18, 0) NULL,
+	[12] [numeric](18, 0) NULL
+)
+GO
