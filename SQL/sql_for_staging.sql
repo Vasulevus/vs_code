9b58f2a9281dfe8f245df0464eec7dde9593c9a1
Vasulevus <Vasulevus@gmail.com>
2022-07-02 18:30:09 +0300

    drop table and add table

---
+++ SQL/sql_for_staging.sql
@@ -0,0 +1,5 @@
+--видалити таблицю, якщо існує
+USE DATAWORK; DROP TABLE IF EXISTS [dbo].[Stage];
+
+--створити таблицю
+USE DATAWORK; CREATE TABLE [dbo].[Stage] ( [1] nvarchar(max) NULL, [2] nvarchar(max) NULL, [3] nvarchar(max) NULL, [4] nvarchar(max) NULL, [5] nvarchar(max) NULL, [6] nvarchar(max) NULL, [7] nvarchar(max) NULL, [8] nvarchar(max) NULL, [9] nvarchar(max) NULL, [10] nvarchar(max) NULL, [11] nvarchar(max) NULL, [12] nvarchar(max) NULL, [13] nvarchar(max) NULL, [14] nvarchar(max) NULL, [15] nvarchar(max) NULL, [16] nvarchar(max) NULL, [17] nvarchar(max) NULL, [18] nvarchar(max) NULL, [19] nvarchar(max) NULL, [20] nvarchar(max) NULL, [21] nvarchar(max) NULL, [22] nvarchar(max) NULL, [23] nvarchar(max) NULL, [24] nvarchar(max) NULL, [25] nvarchar(max) NULL, [26] nvarchar(max) NULL, [27] nvarchar(max) NULL, [28] nvarchar(max) NULL, [29] nvarchar(max) NULL, [30] nvarchar(max) NULL)
