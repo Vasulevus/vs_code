1f0c65d67c53dc18b73e05e234571b91f136b06b
Vasulevus <Vasulevus@gmail.com>
2022-08-11 17:13:33 +0300

    add rdl

---
+++ SQL/2.sql
@@ -0,0 +1,10 @@
+CREATE TABLE [ZipCode] (
+[StateFIPCode] smallint,
+[ZipCode] varchar(5),
+[StateAbbr] varchar(2),
+[City] varchar(16),
+[Longitude] real,
+[Latitude] real,
+[Population] int,
+[AllocationPercentage] real
+)
