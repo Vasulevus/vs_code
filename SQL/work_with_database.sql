4040b8dc1d12ddff71663e111c7d5703c5017b52
Vasulevus <Vasulevus@gmail.com>
2022-09-02 11:31:25 +0300

    work with

--- SQL/work_with_database.sql
+++ SQL/work_with_database.sql
@@ -24,4 +24,29 @@ ALTER DATABASE AFT --ЗМІНА БАЗИ ДАНИХ
 						SIZE = 7,
 						MAXSIZE = 100,
 						FILEGROWTH = 5%
-					)
+					);
+ALTER DATABASE AFT
+		MODIFY FILE --ЗМІНЮЄМО ФАЙЛ
+				(NAME = AFT_FILE, SIZE = 15);
+
+CREATE TABLE customers
+				(
+					customerid CHAR(5) NOT NULL,
+					companyname VARCHAR(40) NOT NULL,
+					contactname CHAR(30) NULL,
+					[address] VARCHAR(60) NULL,
+					city CHAR(15) NULL,
+					phone CHAR(24) NULL,
+					fax CHAR(24) NULL
+				);
+CREATE TABLE orders
+				(
+					orderid INTEGER NOT NULL,
+					customerid CHAR(5) NOT NULL,
+					orderdate DATE NULL,
+					shippeddate DATE NULL,
+					freight MONEY NULL,
+					shipname VARCHAR(40) NULL,
+					shipaddress VARCHAR(60) NULL,
+					quantity INTEGER NULL
+				);
