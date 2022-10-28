fa178d3cd33cb22fcb71734645b1a57197866be4
Vasulevus <Vasulevus@gmail.com>
2022-10-28 14:21:35 +0300

    variant of 777_output3

--- SQL/output_form/777.output_form3.sql
+++ SQL/output_form/777.output_form3.sql
@@ -18,40 +18,33 @@ BEGIN
 
     BEGIN
         USE [db_archive] -- для тестування, закоментувати на прод
-        SELECT
-             [F931084159] AS [П.н.]
-            ,[F_58852227] AS [Показник]
-            ,[F_360781186] AS [ВОУ Пн ТУОМ]
-            , NULL AS [ВСП Пн ТУОМ]
-            , NULL AS [ВОУ Пд ТУОМ]
-            , NULL AS [ВСП Пд ТУОМ]
-        FROM [777].[input_form1]
-        UNION ALL
-        SELECT
-             [F_2105900422] AS [П.н.]
-            ,[F2105179514] AS [Показник]
-            ,NULL AS [ВОУ Пн ТУОМ]
-            , [F1920758396]AS [ВСП Пн ТУОМ]
-            , NULL AS [ВОУ Пд ТУОМ]
-            , NULL AS [ВСП Пд ТУОМ]
-        FROM [777].[input_form2]
-        UNION ALL
-        SELECT
-             [F931084159] AS [П.н.]
-            ,[F_58852227] AS [Показник]
-            ,NULL AS [ВОУ Пн ТУОМ]
-            , NULL AS [ВСП Пн ТУОМ]
-            ,[F_360781186] AS [ВОУ Пд ТУОМ]
-            , NULL AS [ВСП Пд ТУОМ]
-        FROM [777].[input_form3] 
-        UNION ALL
-        SELECT
-             [F931084159] AS [П.н.]
-            ,[F_58852227] AS [Показник]
-            ,NULL AS [ВОУ Пн ТУОМ]
-            , NULL AS [ВСП Пн ТУОМ]
-            ,NULL AS [ВОУ Пд ТУОМ]
-            ,[F_360781186] AS [ВСП Пд ТУОМ]
-        FROM [777].[input_form4] 
+ 	   SELECT
+	[П.н.]
+	,[Показник]
+	,[ВОУ Пд ТУОМ]
+	,[ВСП Пд ТУОМ]
+	,CASE WHEN [П.н.] IN (4, 5, 9, 11, 13, 14, 15, 16, 17, 18, 19, 20, 53, 63, 64) THEN 
+	TRY_CAST([ВОУ Пд ТУОМ] AS decimal(15,3)) + TRY_CAST([ВСП Пд ТУОМ] AS decimal(15,3))
+	ELSE NULL END AS [Південний ТУОМ (всього)]
+	FROM  
+     (   SELECT
+            [IF1].[F931084159] AS [П.н.]
+            ,[IF1].[F_58852227] AS [Показник]
+            ,[IF1].[F_360781186] AS [ВОУ Пн ТУОМ]
+         --   ,[IF2].[F1920758396]AS [ВСП Пн ТУОМ] 
+            ,[IF3].[F_360781186] AS [ВОУ Пд ТУОМ]
+            ,[IF4].F_360781186 AS [ВСП Пд ТУОМ]
+        FROM [777].[input_form1] AS IF1
+ --       JOIN
+--		[777].[input_form2] AS IF2
+	--	ON IF1.F931084159 = IF2.F2105179514
+		JOIN
+		[777].[input_form3] AS IF3
+		ON IF1.F931084159 = IF3.F931084159
+		JOIN
+		[777].[input_form4] AS IF4
+		ON IF1.F931084159 = IF4.F931084159
+		) AS A
+
     END
 END
