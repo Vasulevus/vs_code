b65fda3feaa39d0f3cce60c96ca14267e04e7af6
Vasulevus <Vasulevus@gmail.com>
2022-12-08 12:22:52 +0200

    powershell update

--- powershell/create_folders.ps1
+++ powershell/create_folders.ps1
@@ -1,17 +1,25 @@
-$Path = 'C:\Users\Asus\Documents\Work'#шлях
-$Folder = '0147' #назва макету/папка
-Set-Location $Path #переміщуємося на потрібну точку
-New-Item -ItemType Directory -Path $Folder #створюємо папку з назвою макета
-Set-Location $Path\$Folder #переміщуємося в новстворену папку
-New-Item -ItemType Directory -Path 'Excel' #створюємо додаткові стандартні папки
-New-Item -ItemType Directory -Path 'PBI'
-New-Item -ItemType Directory -Path 'RDL'
-New-Item -ItemType Directory -Path 'SSIS'
-New-Item -ItemType Directory -Path 'Archive'
-New-Item -ItemType Directory -Path 'Historic_Data'
-New-Item -ItemType Directory -Path 'CSV_OUTPUT'
-New-Item -ItemType Directory -Path 'TECH_Docs'
-Set-Location $Path\$Folder\Historic_Data #переміщуємося в новстворену папку для історичних даних
-New-Item -ItemType Directory -Path 'CSV' #створюємо додаткові стандартні папки
-New-Item -ItemType Directory -Path 'EXCEL'
-New-Item -ItemType Directory -Path 'DBF'
+$Path = 'C:\Users\Asus\Documents\Work'#змінна для шляху
+$Folder = '0147' #змінна для назви макету/папка
+$FolderPath = $Path + "\" + $Folder
+Set-Location $Path #переміщуємося на потрібну точку 
+IF (Test-Path $FolderPath){
+    Write-Host "Folder Exists"
+#    Remove-Item $FolderPath -Recurse #розкоментити, щоб видалялася папка з усіма підпапками
+}ELSE{
+№    Write-Host "Folder Doesn`t Exists"
+    New-Item -ItemType Directory -Path $Path\$Folder #створюємо папку з назвою макета
+#    Set-Location $Path\$Folder ##розкоментити, щоб переміщуватися в новстворену папку
+    New-Item -ItemType Directory -Path $Path\$Folder'\Excel' #створюємо додаткові стандартні папки
+    New-Item -ItemType Directory -Path $Path\$Folder'\PBI'
+    New-Item -ItemType Directory -Path $Path\$Folder'\RDL'
+    New-Item -ItemType Directory -Path $Path\$Folder'\SSIS'
+    New-Item -ItemType Directory -Path $Path\$Folder'\Archive'
+    New-Item -ItemType Directory -Path $Path\$Folder'\Historic_Data'
+    New-Item -ItemType Directory -Path $Path\$Folder'\CSV_OUTPUT'
+    New-Item -ItemType Directory -Path $Path\$Folder'\TECH_Docs'
+#    Set-Location $Path\$Folder\Historic_Data #розкоментити, щоб переміщуватися в новстворену папку
+    New-Item -ItemType Directory -Path $Path\$Folder'\Historic_Data\CSV' #створюємо додаткові стандартні папки
+    New-Item -ItemType Directory -Path $Path\$Folder'\Historic_Data\EXCEL'
+    New-Item -ItemType Directory -Path $Path\$Folder'\Historic_Data\DBF'
+    Write-Host "Створено нові папки"
+}
